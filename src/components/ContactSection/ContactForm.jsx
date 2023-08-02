4927fdb81fb13fad560e1a598b892b4116476009
Murphy <109974838+Murphy120814@users.noreply.github.com>
2023-08-02 08:29:55 +0530

    version 1.0.3
    slight changes in form if this work then fine otherwise haveto change the form

--- src/components/ContactSection/ContactForm.jsx
+++ src/components/ContactSection/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 function ContactForm() {
@@ -21,68 +21,68 @@ function ContactForm() {
         message: '',
       }}
       validationSchema={validationSchema}
-      onSubmit={(values) => {
-        // , { setSubmitting }
+      onSubmit={(values, { setSubmitting }) => {
+        //
         // Netlify Forms will handle the form submission automatically.
         // You don't need to use Axios or custom serverless functions for this setup.
-        // setSubmitting(false);
+        setSubmitting(false);
       }}
     >
-      {/* {({ isSubmitting }) => ( */}
-      <Form
-        name="contact"
-        netlify
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        className="contact form"
-        method="post"
-      >
-        <h3>Send a Message:</h3>
-        <div className="formBox">
-          <div className="row50">
-            <div className="inputBox">
-              <label htmlFor="name">Name</label>
-              <Field type="text" name="name" id="name" />
-              <ErrorMessage name="name" component="div" className="error-message" />
+      {({ isSubmitting }) => (
+        <form
+          name="contact"
+          netlify
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          className="contact form"
+          method="post"
+        >
+          <h3>Send a Message:</h3>
+          <div className="formBox">
+            <div className="row50">
+              <div className="inputBox">
+                <label htmlFor="name">Name</label>
+                <Field type="text" name="name" id="name" />
+                <ErrorMessage name="name" component="div" className="error-message" />
+              </div>
+
+              <div className="inputBox">
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" id="email" />
+                <ErrorMessage name="email" component="div" className="error-message" />
+              </div>
+            </div>
+            <div className="row50">
+              <div className="inputBox">
+                <label htmlFor="jobRole">Job Role</label>
+                <Field type="text" name="jobRole" id="jobRole" />
+                <ErrorMessage name="jobRole" component="div" className="error-message" />
+              </div>
+
+              <div className="inputBox">
+                <label htmlFor="companyName">Company Name</label>
+                <Field type="text" name="companyName" id="companyName" />
+                <ErrorMessage name="companyName" component="div" className="error-message" />
+              </div>
             </div>
 
-            <div className="inputBox">
-              <label htmlFor="email">Email</label>
-              <Field type="email" name="email" id="email" />
-              <ErrorMessage name="email" component="div" className="error-message" />
+            <div className="row100">
+              <div className="inputBox">
+                <label htmlFor="message">Message</label>
+                <Field as="textarea" name="message" id="message" />
+                <ErrorMessage name="message" component="div" className="error-message" />
+              </div>
+            </div>
+
+            <div className="row100">
+              <div className="inputBox">
+                <input name="submit" type="submit" value="Send" disabled={isSubmitting} />
+              </div>
             </div>
           </div>
-          <div className="row50">
-            <div className="inputBox">
-              <label htmlFor="jobRole">Job Role</label>
-              <Field type="text" name="jobRole" id="jobRole" />
-              <ErrorMessage name="jobRole" component="div" className="error-message" />
-            </div>
 
-            <div className="inputBox">
-              <label htmlFor="companyName">Company Name</label>
-              <Field type="text" name="companyName" id="companyName" />
-              <ErrorMessage name="companyName" component="div" className="error-message" />
-            </div>
-          </div>
-
-          <div className="row100">
-            <div className="inputBox">
-              <label htmlFor="message">Message</label>
-              <Field as="textarea" name="message" id="message" />
-              <ErrorMessage name="message" component="div" className="error-message" />
-            </div>
-          </div>
-
-          <div className="row100">
-            <div className="inputBox">
-              <input name="submit" type="submit" value="Send" />
-            </div>
-          </div>
-        </div>
-
-      </Form>
-      {/* )} */}
+        </form>
+      )}
     </Formik>
   );
 }
